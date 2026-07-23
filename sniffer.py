8876bd77e8a254d71af66580d01f5a5c1fddb00a
YuvalN1211 <yuvaln2010@gmail.com>
2026-07-23 22:36:10 +0300

    finish

--- sniffer.py
+++ sniffer.py
@@ -1,16 +1,17 @@
 import scapy.all as scapy
 from scapy.layers import http
 
-interface = "Realtek PCIe GbE Family Controller"
+my_ethernet_interface = "Realtek PCIe GbE Family Controller"
+my_loopback_interface = "Loopback Pseudo-Interface 1"
 
 def sniff(interface):
     scapy.sniff(iface = interface, store = False, prn = process_packet)
 
 def get_url(packet):
-    print(packet)
-    return (packet[http.HTTPRequest].host + packet[http.HTTPRequest].Path).decode("utf-8")
+    return (packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path).decode("utf-8")
 
 def process_packet(packet):
+    print(f"sniffing packet: {packet}")
     if packet.haslayer(http.HTTPRequest):
         url = get_url(packet)
         print(f"HTTP url is: {url}")
@@ -24,7 +25,7 @@ def get_credentials(packet):
     if packet.haslayer(scapy.Raw):
         payload = packet[scapy.Raw].load.decode("utf-8")
         for keyword in keywords:
-            if keyword in payload:
+            if keyword in payload.lower():
                 return payload
 
-sniff(interface)
+sniff(my_loopback_interface)
